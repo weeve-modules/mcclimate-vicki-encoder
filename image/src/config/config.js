443cb2136b54f9e4fdba0f913c537bd857a280c2
mesud pasic <839950+mesudpasic@users.noreply.github.com>
2022-04-30 15:54:04 +0200

    modified encoder to support being setup in pipeline after scheduler service and before melita-io service

--- image/src/config/config.js
+++ image/src/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   INGRESS_HOST: env('INGRESS_HOST', '127.0.0.1'),
   INGRESS_PORT: env('INGRESS_PORT', '8082'),
   MODULE_NAME: env('MODULE_NAME', 'McClimate Vicki Encoder'),
-  EGRESS_URL: env('EGRESS_URL', ''),
+  EGRESS_URL: env('EGRESS_URL', 'http://localhost:8083'),
   EXECUTE_SINGLE_COMMAND: env('EXECUTE_SINGLE_COMMAND', 'no'),
   SINGLE_COMMAND: env('SINGLE_COMMAND', 'setTargetTemperature'),
 }
