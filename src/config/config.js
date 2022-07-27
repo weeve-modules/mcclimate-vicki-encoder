9c2eba2241a39fe3d50b15f2aaccfa0cb93d68ac
mesud pasic <839950+mesudpasic@users.noreply.github.com>
2022-07-27 22:52:47 +0200

    category updated

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const env = require('../utils/env')
 
 module.exports = {
   INGRESS_HOST: env('INGRESS_HOST', '127.0.0.1'),
-  INGRESS_PORT: env('INGRESS_PORT', '8083'),
+  INGRESS_PORT: env('INGRESS_PORT', '8080'),
   MODULE_NAME: env('MODULE_NAME', 'McClimate Vicki Encoder'),
   EGRESS_URLS: env('EGRESS_URLS', ''),
   EXECUTE_SINGLE_COMMAND: env('EXECUTE_SINGLE_COMMAND', 'no'),
