ac2ccc9d26466709cebd5d360f94a4d118a64256
mesud pasic <839950+mesudpasic@users.noreply.github.com>
2022-08-02 14:56:09 +0200

    updates.

--- src/utils/encoder.js
+++ src/utils/encoder.js
@@ -275,9 +275,8 @@ const hexToBase64 = hexstring => {
 
 const send = async result => {
   if (EGRESS_URLS) {
-    const urls = []
     const eUrls = EGRESS_URLS.replace(/ /g, '')
-    urls.push(...eUrls.split(','))
+    const urls = eUrls.split(',')
     urls.forEach(async url => {
       if (url) {
         try {
